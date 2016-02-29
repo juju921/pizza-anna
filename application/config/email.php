eb5ccfa0711e97a2b07ad4730739d8927177bf40
julien <julien.garretb@gmail.com>
2016-02-29 11:00:59 +0100

    Update email.php

--- application/config/email.php
+++ application/config/email.php
@@ -3,7 +3,7 @@
 $config['protocol'] = 'smtp';
 $config['smtp_host'] = 'ssl://smtp.gmail.com';
 $config['smtp_port'] = '465';
-$config['smtp_user'] = 'julien.garretb@gmail.com';
-$config['smtp_pass'] = 'azertyui';
+$config['smtp_user'] = '';
+$config['smtp_pass'] = '';
 $config['charset'] = 'utf-8';
 $config['newline'] = "\r\n";
